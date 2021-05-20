38ea249168b5d6ba7b1f7465d89b20f2ca3a947a
Justin Rodriguez <justinrodriguez@live.com>
2021-05-20 06:29:42 -0700

    Game can now see which player has won and detect if the game is a draw

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -54,10 +54,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         playerTurn.setText("Player X's Turn");
+        player = "X";
+    }
+
+    public void gameWon(View v) {
+
+        Button[] list = new Button[]{topLeftCmd, topMiddleCmd, topRightCmd, middleLeftCmd,
+                centerCmd, middleRightCmd, bottomLeftCmd, bottomCenterCmd, bottomRightCmd};
+        int count = 0;
+
+        if(playerWonGame()) {
+            for (int i = 0; i < list.length; i++) {
+                if(list[i].getText().equals("")) {
+                    list[i].setClickable(false);
+                }
+            }
+        }
+        else {
+            for (int i = 0; i < list.length; i++) {
+                if(!list[i].getText().equals("")) {
+                    count++;
+                    if(count >= 9) {
+                        playerTurn.setText("Game is a draw");
+                        count = 0;
+                    }
+                }
+            }
+
+        }
     }
 
     public void turnSubmitted(View v) {
         Button buttonClicked = findViewById(v.getId());
+
         if(player.equals("X")) {
             buttonClicked.setText("X");
             buttonClicked.setClickable(false);
@@ -70,5 +99,63 @@ public class MainActivity extends AppCompatActivity {
             playerTurn.setText("Player X's Turn");
             player = "X";
         }
+
+        gameWon(v);
+    }
+
+    public boolean playerWonGame() {
+        Button[][] list = new Button[][] {{topLeftCmd, topMiddleCmd, topRightCmd},
+                                          {middleLeftCmd, centerCmd, middleRightCmd},
+                                          {bottomLeftCmd, bottomCenterCmd, bottomRightCmd}};
+        //TicTacToe Horizontal row
+        for(int i = 0; i < list.length; i++) {
+            for(int j = 0; j < list[i].length - 2; j++) {
+                if(list[i][j].getText().equals("X") || list[i][j].getText().equals("O")) {
+                    if (list[i][j].getText().equals(list[i][j + 1].getText()) &&
+                            list[i][j].getText().equals(list[i][j + 2].getText())) {
+                        playerTurn.setText("Player " + list[i][j].getText() + " has Won");
+                        return true;
+                    }
+                }
+            }
+        }
+        //TicTacToe Vertical row
+        for(int i = 0; i < list.length - 2; i++) {
+            for (int j = 0; j < list[i].length; j++) {
+                if (list[i][j].getText().equals("X") || list[i][j].getText().equals("O")) {
+                    if (list[i][j].getText().equals(list[i + 1][j].getText()) &&
+                            list[i][j].getText().equals(list[i + 2][j].getText())) {
+                        playerTurn.setText("Player " + list[i][j].getText() + " has Won");
+                        return true;
+                    }
+                }
+            }
+        }
+        //TicTacToe Down Diagonal row
+        for(int i = 0; i < list.length - 2; i++) {
+            for (int j = 0; j < list[i].length - 2; j++) {
+                if (list[i][j].getText().equals("X") || list[i][j].getText().equals("O")) {
+                    if (list[i][j].getText().equals(list[i + 1][j + 1].getText()) &&
+                            list[i][j].getText().equals(list[i + 2][j + 2].getText())) {
+                        playerTurn.setText("Player " + list[i][j].getText() + " has Won");
+                        return true;
+                    }
+                }
+            }
+        }
+        //TicTacToe Up Diagonal row
+        for(int i = 0; i < list.length - 2; i++) {
+            for (int j = 0; j < list[i].length - 2; j++) {
+                if (list[i + 2][j].getText().equals("X") || list[i + 2][j].getText().equals("O")) {
+                    if (list[i + 2][j].getText().equals(list[i + 1][j + 1].getText()) &&
+                            list[i + 2][j].getText().equals(list[i][j + 2].getText())) {
+                        playerTurn.setText("Player " + list[i + 2][j].getText() + " has Won");
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
